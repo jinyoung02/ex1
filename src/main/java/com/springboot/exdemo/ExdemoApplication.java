6553d6d5009ca2356c75cc87ae3de316a371e17e
jinyoung02 <jink8962@naver.com>
2025-04-26 13:41:36 +0900

    commit 1

---
+++ src/main/java/com/springboot/exdemo/ExdemoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.exdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/exdemo/ExdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.exdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
